38556d2a34e778e733ac7d020c5b7dec1c64f732
邹杭 <@DESKTOP-8B4KT98>
2018-01-19 18:13:12 +0800

    你好

---
+++ src/cn/edu/neu/service/UserService.java
@@ -0,0 +1,16 @@
+package cn.edu.neu.service;
+
+import cn.edu.neu.core.common.Page;
+
+import cn.edu.neu.model.User;
+
+
+public interface UserService {
+	User existsUser(User user);
+	boolean addUser(User user);
+	boolean existsUserName(String userName);
+	User existsAdmin(User user);
+	User getPersonalInfo(int loginUserId);
+	void updatePersonalInfo(User user);
+	void updatePersonalPassword(User user);
+}
